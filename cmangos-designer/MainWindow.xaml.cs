6f862aca66e69eec4b2452b8c6418faf6e94cdbc
killerwife <killerwife@gmail.com>
2022-05-27 14:09:26 +0200

    Enable setting window size

--- cmangos-designer/MainWindow.xaml.cs
+++ cmangos-designer/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using cmangos_designer.Designers;
 using cmangos_designer.Helpers;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Runtime.InteropServices;
+using WinRT;
+using Windows.ApplicationModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,18 +17,40 @@ namespace cmangos_designer
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private IntPtr m_windowHandle;
+        public IntPtr WindowHandle { get { return m_windowHandle; } }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
+        }
+
         public MainWindow()
         {
             this.InitializeComponent();
             if (Content is FrameworkElement rootElement) // designed it in dark theme - remove this if someone adds light theme design - also change App
                 rootElement.RequestedTheme = ElementTheme.Dark;
-            contentFrame.Navigate(typeof(DbscriptsDesigner));
+            contentFrame.Navigate(typeof(EAIDesigner));
+
+            var windowNative = this.As<IWindowNative>();
+            m_windowHandle = windowNative.WindowHandle;
+            SetWindowSize(m_windowHandle, 1300, 850);
         }
 
-        //private void myButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    myButton.Content = "Clicked";
-        //}
+        private void SetWindowSize(IntPtr hwnd, int width, int height)
+        {
+            var dpi = PInvoke.User32.GetDpiForWindow(hwnd);
+            float scalingFactor = (float)dpi / 96;
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            PInvoke.User32.SetWindowPos(hwnd, PInvoke.User32.SpecialWindowHandles.HWND_TOP,
+                                        0, 0, width, height,
+                                        PInvoke.User32.SetWindowPosFlags.SWP_NOMOVE);
+        }
 
         private void buttonExit_Click(object sender, RoutedEventArgs e)
         {
